de84c94b75b5575f8aafc837c586e6ccbd5568c2
MonikaAgarwal <monika.agarwal@atmecs.com>
2015-04-01 12:29:45 +0530

    Added var x

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -3,8 +3,9 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x=3;
 System.out.println("test....class");
-System.out.println("test....");
+System.out.println(x);
 	}
 
 }
